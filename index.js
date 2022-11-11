9000152d025108770a63db671107fd73317887a2
suleman-swe <117747893+suleman-swe@users.noreply.github.com>
2022-11-11 05:24:56 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+/*a=21;
+if(a<18)
+{
+    document.write("Welcome, You are allowed. Enjoy Your");
+}else{
+    document.write("You's are is not matching. Your are not allowed!");
+}*/
+
+//IF-ELSE
+
+/*x = 21;
+if (x <= 18) {
+    document.write("You are not 18");
+} else {
+    document.write("You are : " + x);
+}*/
+
+//TERNARY OPERATOR
+
+/*var a = "3";
+var b;
+
+//(a = 3) ? b = "Khan" : b = "Not Available";       //OR
+//b = (a == 3) ? "Khan" : "Not Available";
+document.write(b);*/
+
+//SWITCH CASE
+
+/*var a = 6;   //Ex(1)
+switch(a){
+    case 1 :
+        document.write("Number is "+a);
+    break;
+    case 2 :
+        document.write("Number is "+a);
+    break;
+    case 3 :
+        document.write("Number is "+a);
+    break;
+    
+    default:
+        document.write('"'+a+'"'+" number is not in the list");
+}*/
+
+/*var age = 21;   //Ex(2)
+switch(true){
+    case(age<=17):
+        document.write("Younger! Sorry this scholarship is not for you.");
+    break;
+    case(age>=18 && age<=25):
+        document.write("Yeah! You are eligible for this scholarship");
+    break;
+
+    default:
+        document.write("Sorry elder boy. You are not eligible for scholarship. Go get a job!");
+}*/
+
+var a = prompt("First Number");
+//var b = prompt("Second Number");
+//console.log("Fisrt user input is: " + a);
+//console.log("Fisrt user input is: " + b);
+//var c = a+b;
+console.log(a);
+console.log(typeof a);
+
+var b = Number(a);
+console.log(b);
+console.log(typeof b);
