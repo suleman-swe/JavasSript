5b3f5c9eb9acaaa50077076c44f7e52e492d8dd7
Suleman Khan <suleman.swe@gmail.com>
2023-04-01 16:25:44 +0500

    Update problems.js

--- problems.js
+++ problems.js
@@ -1,6 +1,6 @@
 //Write a JavaScript program to check two numbers and return true if one of the number is 100 or if the sum of two numbers is 100
 
-//Solution 1
+//Solution # 1
 var a = parseInt(prompt("Enter first value: "));
 var b = parseInt(prompt("Enter second value: "));
 if (a == 100 || b == 100) {
@@ -11,7 +11,7 @@ if (a == 100 || b == 100) {
     console.log(false);
 }
 
-//Solution 2
+//Solution # 2
 var checkIfEqualTo100 = (a, b) => {
     if (a == 100 || b == 100) {
         return true;
@@ -21,7 +21,7 @@ var checkIfEqualTo100 = (a, b) => {
 }
 console.log(checkIfEqualTo100(100, 0));
 
-//Solution 3
+//Solution # 3
 var checkIfEqualTo100 = (a, b) => a == 100 || b == 100;
 
 console.log(checkIfEqualTo100(2, 100));
